ffa1862ddcc4aa7f8085af769eabeecab3fabaa0
KeithLord <ismaelcerrudo.12@gmail.com>
2022-10-05 09:33:49 -0300

    Fetch

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-const stockProductos = [
-    {id: 1,tipo:'heladera', nombre: 'Heladera', precio: 19999, img: '../img/heladera.png'},
-    {id: 2,tipo:'heladera', nombre: 'Heladera 2', precio: 39999, img: '../img/heladera.png'},
-    {id: 3,tipo:'heladera', nombre: 'Heladera 3', precio: 59999, img: '../img/heladera.png'},
-    {id: 4,tipo:'horno', nombre: 'Horno ', precio: 29999, img: '../img/horno.png'},
-    {id: 5,tipo:'horno', nombre: 'Horno 2', precio: 49999, img: '../img/horno.png'},
-    {id: 6,tipo:'horno', nombre: 'Horno 3', precio:28900, img: '../img/horno.png'},
-    {id: 7,tipo:'tostadora', nombre: 'Tostadora', precio: 35000, img: '../img/tostadora.png'},
-    {id: 8,tipo:'tostadora', nombre: 'Tostadora 2', precio:39000, img: '../img/tostadora.png'},
-    {id: 9,tipo:'tostadora', nombre: 'Tostadora 3', precio:49999, img: '../img/tostadora.png'},
-    {id: 10,tipo:'cafetera', nombre: 'Cafetera', precio: 12000, img: '../img/cafetera.png'},
-    {id: 11,tipo:'cafetera', nombre: 'Cafetera 2', precio:10000, img: '../img/cafetera.png'},
-    {id: 12,tipo:'cafetera', nombre: 'Cafetera 3', precio:31200, img: '../img/cafetera.png'}
-]
+// const stockProductos = [
+//     {id: 1,tipo:'heladera', nombre: 'Heladera', precio: 19999, img: '../img/heladera.png'},
+//     {id: 2,tipo:'heladera', nombre: 'Heladera 2', precio: 39999, img: '../img/heladera.png'},
+//     {id: 3,tipo:'heladera', nombre: 'Heladera 3', precio: 59999, img: '../img/heladera.png'},
+//     {id: 4,tipo:'horno', nombre: 'Horno ', precio: 29999, img: '../img/horno.png'},
+//     {id: 5,tipo:'horno', nombre: 'Horno 2', precio: 49999, img: '../img/horno.png'},
+//     {id: 6,tipo:'horno', nombre: 'Horno 3', precio:28900, img: '../img/horno.png'},
+//     {id: 7,tipo:'tostadora', nombre: 'Tostadora', precio: 35000, img: '../img/tostadora.png'},
+//     {id: 8,tipo:'tostadora', nombre: 'Tostadora 2', precio:39000, img: '../img/tostadora.png'},
+//     {id: 9,tipo:'tostadora', nombre: 'Tostadora 3', precio:49999, img: '../img/tostadora.png'},
+//     {id: 10,tipo:'cafetera', nombre: 'Cafetera', precio: 12000, img: '../img/cafetera.png'},
+//     {id: 11,tipo:'cafetera', nombre: 'Cafetera 2', precio:10000, img: '../img/cafetera.png'},
+//     {id: 12,tipo:'cafetera', nombre: 'Cafetera 3', precio:31200, img: '../img/cafetera.png'}
+// ]
 
 
 const container = document.querySelector(".container");
@@ -21,9 +21,11 @@ const tostadoras = document.querySelector('.tostadoras');
 const cafeteras = document.querySelector('.cafeteras');
 const heladeras = document.querySelector('.heladeras');
 const hornos = document.querySelector(".hornos");
+const iconCart = document.querySelector('.iconCart');
+const totalPagos = document.querySelector('.totalPagos');
 const carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 //Funcion creadora de cards
-function crearCards(array){
+function crearCards(array) {
     container.innerHTML = ''
     array.forEach(product => {
         container.innerHTML += `<div class="card d-flex flex-column ">
@@ -36,42 +38,54 @@ function crearCards(array){
     agregarFuncionAlBoton(array);
 }
 //Funcion al apretar cualquier boton se ejecute agregarAlCarrito
-function agregarFuncionAlBoton(array){
-    array.forEach(product =>{
-        document.querySelector(`#btn-agregar${product.id}`).addEventListener('click', ()=>{
+function agregarFuncionAlBoton(array) {
+    array.forEach(product => {
+        document.querySelector(`#btn-agregar${product.id}`).addEventListener('click', () => {
             Toastify({
 
                 text: `Se agrego una unidad de ${product.nombre}`,
-                
-                duration: 3000
-                
-                }).showToast();
+
+                duration: 1000
+
+            }).showToast();
             agregarAlCarrito(product);
-            console.log(product)
-        }) 
+            
+        })
     })
 }
 //Funcion agregar al carrito 
-function agregarAlCarrito(product){
+function agregarAlCarrito(product) {
     let existe = carrito.some(el => el.id === product.id);
-    if (existe === false){
+    if (existe === false) {
         product.cantidad = 1;
         carrito.push(product);
-    }else{
+    } else {
         let prodFind = carrito.find(el => el.id === product.id);
         prodFind.cantidad++;
-        //forma de sumar precio sin el map
-        product.precio = product.precio + product.precio;
 
         // sumarPrecioCarrito();
     }
-    console.log(carrito);
     renderCarrito();
-    
+
 }
-function renderCarrito(){
+iconCart.addEventListener('click',()=>{
+    carritoDiv.classList.toggle("active");
+})
+//Hacer Calculo Final con el monto total
+// function totalPago(carrito){
+//     const monto = carrito.map(producto =>{
+//         producto.total = producto.precio * producto.cantidad
+//         console.log(producto.total)
+
+//         // totalPagos.innerHTML = `<ul class="filaCarrito d-flex flex-wrap justify-content-evenly mt-4">
+//         // <li></li>
+//         // </ul>`
+//     })
+// }
+//Recorrer el array del carrito y por cada producto crear las filas del carrito, tambien guardando en el storage los datos
+function renderCarrito() {
     carritoDiv.innerHTML = ``;
-    carrito.forEach(product =>{
+    carrito.forEach(product => {
         carritoDiv.innerHTML += `<ul class="filaCarrito d-flex flex-wrap justify-content-evenly mt-4">
         <li class="p-3">${product.nombre}</li>
         <li class="p-3">$${product.precio}</li>
@@ -81,57 +95,64 @@ function renderCarrito(){
     })
     localStorage.setItem("carrito", JSON.stringify(carrito))
     funcionEliminar();
+    // totalPago(carrito);
+    
 }
- function funcionEliminar() {
-     carrito.forEach(product =>{
-         document.querySelector(`#btn-eliminar${product.id}`).addEventListener('click', ()=>{
+
+function funcionEliminar() {
+    carrito.forEach(product => {
+        document.querySelector(`#btn-eliminar${product.id}`).addEventListener('click', () => {
             //LLamar a tostify
             Toastify({
 
                 text: `Se elimino con exito el producto ${product.nombre}`,
-                
-                duration: 3000
-                
-                }).showToast();
-             let indice = carrito.findIndex(el => el.id === product.id);
-             carrito.splice(indice, 1);
-             renderCarrito();
-         })
-     })
- }
+
+                duration: 1000
+
+            }).showToast();
+            let indice = carrito.findIndex(el => el.id === product.id);
+            carrito.splice(indice, 1);
+            renderCarrito();
+        })
+    })
+}
+
+
+
+
+//Aplicando fetch para consumir el archivo json
+
+const stockProductos = async () => {
+    const response = await fetch('./js/data.json');
+    const data = await response.json();
+    crearCards(data);
+    filtrar(data);
+}
+
 //Filtrados
- hornos.addEventListener('click', (product) => {
-    const filtroHorno = stockProductos.filter(el => el.tipo === 'horno');
-    crearCards(filtroHorno);
- })
+function filtrar(data) {
+    hornos.addEventListener('click', () => {
+        const filtroHorno = data.filter(el => el.tipo === 'horno');
+        crearCards(filtroHorno);
+    })
+    heladeras.addEventListener('click', () => {
+        const filtroHeladeras = data.filter(el => el.tipo === 'heladera');
+        crearCards(filtroHeladeras);
+    })
+    cafeteras.addEventListener('click', () => {
+        const filtroCafeteras = data.filter(el => el.tipo === 'cafetera');
+        crearCards(filtroCafeteras);
+    })
+    tostadoras.addEventListener('click', () => {
+        const filtroTostadora = data.filter(el => el.tipo === 'tostadora');
+        crearCards(filtroTostadora);
+    })
 
- heladeras.addEventListener('click', (product) => {
-    const filtroHeladeras = stockProductos.filter(el => el.tipo === 'heladera');
-    crearCards(filtroHeladeras);
- })
- cafeteras.addEventListener('click', (product) => {
-    const filtroCafeteras = stockProductos.filter(el => el.tipo === 'cafetera');
-    crearCards(filtroCafeteras);
- })
- tostadoras.addEventListener('click', (product) => {
-    const filtroTostadora = stockProductos.filter(el => el.tipo === 'tostadora');
-    crearCards(filtroTostadora);
- })
- 
- all.addEventListener('click', () => {
-    crearCards(stockProductos);
- })
-
-
-//forma sumar precio con array nuevo para no modificar el precio original(En proceso)
-// function sumarPrecioCarrito(){
-//         carrito.map(producto => {
-//         producto.precio = producto.precio + producto.precio;
-//         console.log(producto.precio)
-//     })
-// }
+    all.addEventListener('click', () => {
+        crearCards(data);
+    })
+}
 
 
-
+stockProductos();
 renderCarrito();
-crearCards(stockProductos);
